bda2918fc0f5d3cce1d2fb98c319ee6e4e0fde6b
Douglas <douglaskirimi98@gmail.com>
2022-06-03 18:24:18 +0300

    Solved Errors

--- receive_response.php
+++ receive_response.php
@@ -7,20 +7,28 @@ if($json!=null)
 {
 $data = json_decode($json);
 $res_code = $data->Body->stkCallback->ResultCode;
-if($res_code!=0)
-{
+if($res_code==0) {
  $amount = $data->Body->stkCallback->CallbackMetadata->Item[0]->Value;
  $MpesaReceiptNumber = $data->Body->stkCallback->CallbackMetadata->Item[1]->Value;
  $PhoneNumber = $data->Body->stkCallback->CallbackMetadata->Item[4]->Value;
- $message = "FAILED! You cancelled the payment of Ksh. $amount to JTS. Try again later!";
+ $message = "TRANSACTION SUCCESSFUL! Confirmed payment of Ksh. $amount to JTS completed successfully. Receipt number is $MpesaReceiptNumber";
  $sms->send($PhoneNumber,$message);
 }
-elseif($res_code=0){ 
-	$amount = $data->Body->stkCallback->CallbackMetadata->Item[0]->Value;
-    $MpesaReceiptNumber = $data->Body->stkCallback->CallbackMetadata->Item[1]->Value;
-    $PhoneNumber = $data->Body->stkCallback->CallbackMetadata->Item[4]->Value;
-    $message = "TRANSACTION SUCCESSFUL! Confirmed payment of Ksh. $amount to JTS completed successfully. Receipt number is $MpesaReceiptNumber";
+elseif($res_code==1032){ 
+	$amount = $data->Body->stkCallback->Amount;
+    $PhoneNumber = $data->Body->stkCallback->Msisdn;
+    $message = "FAILED! You cancelled the payment of Ksh. $amount to JTS. Try again later!";
     $sms->send($PhoneNumber,$message);
+  }
+
+elseif($res_code==1037){ 
+	$amount = $data->Body->stkCallback->Amount;
+    $PhoneNumber = $data->Body->stkCallback->Msisdn;
+    $message = "TRANSACTION FAILED! An error occured while processing the request.";
+    $sms->send($PhoneNumber,$message);
+  }
+  else{
+  	
   }
 }
 
