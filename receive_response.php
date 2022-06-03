1ff92793edadbe954ec8d630d20d218853d7d5f3
Douglas <douglaskirimi98@gmail.com>
2022-06-03 17:39:36 +0300

    Fourth commit

--- receive_response.php
+++ receive_response.php
@@ -14,28 +14,13 @@ if($res_code==0)
  $PhoneNumber = $data->Body->stkCallback->CallbackMetadata->Item[4]->Value;
  $message = "Confirmed payment of $amount to Julia Tech Solutions. Receipt number is $MpesaReceiptNumber";
  $sms->send($PhoneNumber,$message);
- 
-//  $sql = "INSERT INTO mpesa (Amount,Phone_Number, Receipt_Number) VALUES ($amount,$PhoneNumber,$MpesaReceiptNumber";
-//  $stmt = $connect->prepare("INSERT INTO mpesa (Amount,Phone_Number, Receipt_Number) VALUES (?,?,?");
-// $stmt->bind_param("sss",$amount,$PhoneNumber,$MpesaReceiptNumber);
-// $stmt->execute();
- 
-//  $query = mysqli_query($connect,"INSERT INTO mpesa (Amount,Phone_Number, Receipt_Number) VALUES ($amount,$PhoneNumber,$MpesaReceiptNumber");
-//  if($query) {
-//      header("Location: index.php?fdb=success");
-//      exit();
-//  }
-//  else{
-//      header("Location: index.php?fdb=error");
-//      exit();
-//  }
 }
 elseif($res_code==1032) { 
 	$amount = $data->Body->stkCallback->CallbackMetadata->Item[0]->Value;
     $MpesaReceiptNumber = $data->Body->stkCallback->CallbackMetadata->Item[1]->Value;
     $PhoneNumber = $data->Body->stkCallback->CallbackMetadata->Item[4]->Value;
     $message = "FAILED! You cancelled the payment of Ksh. $amount to JTS. Try again later!";
- $sms->send($PhoneNumber,$message);
+    $sms->send($PhoneNumber,$message);
   }
 }
 
